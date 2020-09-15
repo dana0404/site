2b163566582c3229e965b62dac444f5e8f5ddfe5
Dana <cortrale@gmail.com>
2020-09-14 20:10:22 -0500

    push

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,7 +45,7 @@ module.exports = {
     {
         resolve: `gatsby-plugin-google-analytics`,
         options: {
-            trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+            trackingId: "UA-173624933-1",
             head: true,
         },
     },
